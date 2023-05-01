eb605ded2405c0fb5653c822682ea6e853c32a0d
Pedro Guarnieri <119369768+pdr606@users.noreply.github.com>
2023-05-01 19:12:59 -0300

    Update App.js

--- financ/src/App.js
+++ financ/src/App.js
@@ -51,6 +51,8 @@ function App() {
       tipo : valueRadio
     }
 
+    console.log(novoObjeto.descricao)
+
 
     setHistorico([...historico, novoObjeto])
     localStorage.setItem('historico', JSON.stringify([...historico, novoObjeto]))
